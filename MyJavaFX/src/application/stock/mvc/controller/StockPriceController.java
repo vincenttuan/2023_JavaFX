b2bb72229f910668818c57a87a823320e9e91f4b
段浩恩 <@DESKTOP-7JSLF7F>
2023-07-23 21:45:06 +0800

    fx

--- MyJavaFX/src/application/stock/mvc/controller/StockPriceController.java
+++ MyJavaFX/src/application/stock/mvc/controller/StockPriceController.java
@@ -1,5 +1,73 @@
 package application.stock.mvc.controller;
 
-public class StockPriceController {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
+import application.stock.mvc.model.StockInfo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+public class StockPriceController {
+	
+	@FXML private TableView<StockInfo> tableView;
+	@FXML private TableColumn<StockInfo, String> symbolCol;
+	@FXML private TableColumn<StockInfo, Double> lastPriceCol;
+	@FXML private TableColumn<StockInfo, String> matchTimeCol;
+	
+	private List<String> symbols = Arrays.asList("2344", "1101", "2330");
+	private ObservableList<StockInfo> stockInfos = FXCollections.observableArrayList();
+	
+	@FXML private void initialize() {
+		
+		symbolCol.setCellValueFactory(new PropertyValueFactory<>("symbol"));
+		lastPriceCol.setCellValueFactory(new PropertyValueFactory<>("lastPrice"));
+		matchTimeCol.setCellValueFactory(new PropertyValueFactory<>("matchTime"));
+		
+		tableView.setItems(getStockData());
+		
+	}
+	
+	private ObservableList<StockInfo> getStockData() {
+		symbols.forEach(symbol -> {
+			StockInfo stockInfo = new StockInfo();
+			stockInfo.setSymbol(symbol);
+			stockInfos.add(stockInfo);
+		});
+		
+		return stockInfos;
+	}
+	
+	public void start(Stage stage) throws IOException {
+		// 載入 fxml
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/StockPriceView.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root, 800, 400);
+		// 設定 css
+		scene.getStylesheets().add(getClass().getResource("../css/stock.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
