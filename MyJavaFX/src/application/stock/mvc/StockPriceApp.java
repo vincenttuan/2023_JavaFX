2b249f8066975cdc1da5e3d1a4ad80799dee318f
段浩恩 <@DESKTOP-7JSLF7F>
2023-07-23 21:47:42 +0800

    fx

---
+++ MyJavaFX/src/application/stock/mvc/StockPriceApp.java
@@ -0,0 +1,23 @@
+package application.stock.mvc;
+
+import application.stock.mvc.controller.StockPriceController;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StockPriceApp extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// 呼叫 controller 的 start() 方法
+		StockPriceController controller = new StockPriceController();
+		controller.start(stage);
+		
+	}
+	
+	
+	
+}
