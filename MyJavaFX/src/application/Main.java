d576b0519cb46631218c3659dc7b8fa6837ae5ce
段浩恩 <@DESKTOP-7JSLF7F>
2023-07-30 22:01:42 +0800

    fx

--- MyJavaFX/src/application/Main.java
+++ MyJavaFX/src/application/Main.java
@@ -1,12 +1,17 @@
 package application;
 	
+import java.io.File;
+import java.io.IOException;
+
+import application.config.PropertiesConfig;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
+	private static final String flagFilePath = PropertiesConfig.PROP.get("flagFilePath")+"";
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +25,16 @@ public class Main extends Application {
 		}
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// 避免二次啟動檔案
+		File flagFile = new File(flagFilePath);
+		if(flagFile.exists()) {
+			System.out.println("請勿重新啟動系統");
+			System.exit(1);
+			return;
+		}
+		flagFile.createNewFile();
+		flagFile.deleteOnExit();
 		launch(args);
 	}
 }
