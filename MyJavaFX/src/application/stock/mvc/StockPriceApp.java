62a594d0b8f154f7d6a36ce5c8b68d0fc3abba1e
段浩恩 <@DESKTOP-7JSLF7F>
2023-08-06 20:05:25 +0800

    java

--- MyJavaFX/src/application/stock/mvc/StockPriceApp.java
+++ MyJavaFX/src/application/stock/mvc/StockPriceApp.java
@@ -4,6 +4,7 @@ package application.stock.mvc;
 import application.stock.mvc.controller.StockPriceController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import socket.client.StockInfoClient;
 
 public class StockPriceApp extends Application {
 	
@@ -13,6 +14,11 @@ public class StockPriceApp extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
+		// 啟用 SocketClient
+		new Thread(() -> {
+			StockInfoClient.main(null);
+		}).start();
+		
 		// 呼叫 controller 的 start() 方法
 		StockPriceController controller = new StockPriceController();
 		controller.start(stage);
