52700ed238dfd4b68ec4bfe9862a7df00cbd08f8
段浩恩 <@DESKTOP-7JSLF7F>
2023-08-13 19:21:01 +0800

    java

--- MyJavaFX/src/socket/model/StockInfo.java
+++ MyJavaFX/src/socket/model/StockInfo.java
@@ -1,9 +1,27 @@
 package socket.model;
 
+/*
+{
+	  "msgSeq":"4324318",
+	  "symbol":"2330  ",
+	  "market":null,
+	  "turnover":6600,
+	  "lastPrice":529.0,
+	  "lastVolume":1,
+	  "bidPrices":[529.0,528.0,527.0,526.0,525.0],
+	  "bidVolumes":[19,687,258,360,388],
+	  "askPrices":[530.0,531.0,532.0,533.0,534.0],
+	  "askVolumes":[859,471,787,650,599],
+	  "matchTime":"131539674237"
+}
+* */
+
 public class StockInfo {
 	private String symbol;
     private Double lastPrice;
     private String matchTime;
+    
+    
     
 	public String getSymbol() {
 		return symbol;
