d54d57e8b6dd71c16eeee903eeebfea9cd302282
段浩恩 <@DESKTOP-7JSLF7F>
2023-08-20 21:54:07 +0800

    fx

--- MyJavaFX/src/application/hello/ExecPythonTest2.java
+++ MyJavaFX/src/application/hello/ExecPythonTest2.java
@@ -9,8 +9,8 @@ public class ExecPythonTest2 {
 	public static void main(String[] args) throws Exception {
         String pythonPath = "python"; 
         String scriptPath = "C:\\Users\\vince\\git\\2023_JavaFX\\MyJavaFX\\src\\application\\hello\\bfp.py"; // 替換為你的Python檔案的路徑
-
-        ProcessBuilder pb = new ProcessBuilder(pythonPath, scriptPath);
+        String stockCode = "2330";
+        ProcessBuilder pb = new ProcessBuilder(pythonPath, scriptPath, stockCode);
         Process process = pb.start();
         
         // 捕捉輸出
--- MyJavaFX/src/application/hello/bfp.py
+++ MyJavaFX/src/application/hello/bfp.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+import sys;
 import twstock
-twstock.__update_codes()
-stock = twstock.Stock('2330')
+#twstock.__update_codes()
+stock_code = sys.argv[1]  # 從命令行參數讀取股票代碼
+stock = twstock.Stock(stock_code)
 bfp = twstock.BestFourPoint(stock)
 print(bfp.best_four_point()) # (False, '量縮價跌, 三日均價由上往下, 三日均價小於六日均價')
