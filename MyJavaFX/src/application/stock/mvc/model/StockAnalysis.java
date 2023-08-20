ec0d91c766cf2462ba4988be3983f4bbdf66f237
段浩恩 <@DESKTOP-7JSLF7F>
2023-08-20 19:29:14 +0800

    fx

--- MyJavaFX/src/application/stock/mvc/model/StockAnalysis.java
+++ MyJavaFX/src/application/stock/mvc/model/StockAnalysis.java
@@ -1,5 +1,15 @@
 package application.stock.mvc.model;
 
+/*
+
+問題: 請問台灣股票代號(stockSymbol)2330.TW的股票名稱(stockName)為何 ? 投資建議敘述(investmentAdvice)? 買進理由(buyingReason)? 賣出理由(sellingReason)?根據買進理由與賣出理由給予投資方向(investmentDirection)?目標價格(targetPrice) ?各為多少,目標價格儘量找到符合市場的價格, 回答請你自由發想, 最後加入投資警語(investmentWarning)
+並用 json 格式回答例如: 
+{
+   "stockSymbol": "2330", "stockName": "台積電", "investmentAdvice": "略..", "buyingReason":"...", "sellingReason":"...", "investmentDirection": "建議買進or建議賣出 or 觀望", "targetPrice": "700.0","investmentWarning": "..."
+}
+最後根據 json 建立一個 StockAnalysis 的 pojo 物件
+
+ * */
 public class StockAnalysis {
 
     private String stockSymbol;
